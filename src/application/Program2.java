b1e3c6f6d28ba6dd0a6554209d21a70c87cfcc98
Hugo VO Moura <zoeiratheforever@gmail.com>
2024-01-01 23:50:56 -0300

    DepartmentDaoJBDC findByAdd implemented

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -12,7 +12,13 @@ public class Program2 {
 		System.out.println("=== TEST 1: Department insert ===");
 		//Department newDepartment = new Department(null, "Cosmetics");
 		//DepartmentDao.insert(newDepartment);
-		System.out.println("=== TEST 2: Department update ===");
+		System.out.println("=== TEST 2: Department findById ===");
+		System.out.println(DepartmentDao.findById(4));
+		System.out.println("=== TEST 3: Department Update ===");
+		Department department = DepartmentDao.findById(1);
+		department.setName("Foods");
+		DepartmentDao.update(department);
+		System.out.println("Update completed");
 	}
 
 }
