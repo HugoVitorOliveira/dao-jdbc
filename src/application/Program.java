8d7c72bdfa308fff2055a31404fcd4dfbee897ef
Hugo VO Moura <zoeiratheforever@gmail.com>
2024-01-01 21:42:45 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,17 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1: Seller findById ===");
 		Seller seller = sellerDao.findById(5);
@@ -36,6 +36,10 @@ public class Program {
 		seller.setName("Marta Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		System.out.println("\n=== TEST 6: Seller Delete ===");
+		System.out.println("Enter a Id for delete test:");
+		sellerDao.deleteById(sc.nextInt());
+		sc.close();
 	
 	}
 }
